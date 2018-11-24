eb4324b0026fbd585bb1821030a2015cdd4b2a0e
Joe Schaar <joe.schaar@gmail.com>
2018-11-24 00:48:14 -0800

    Create untitled0.py
    Initial python file in spyder

---
+++ untitled0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 00:12:35 2018
+
+@author: joe_schaar
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#%%
+t = np.arange(10);
+
+#%%
+plt.plot(t)
+plt.ylabel('some numbers')
+plt.show()
